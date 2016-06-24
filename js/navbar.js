05f9f945bfc668040cd48bed91c064fb513ea0f5
ajadvien@ucalgary.ca <ajadvien@ucalgary.ca>
2016-06-24 14:57:35 -0600

    i give

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-        $('ul.navbar-nav > li').click(function (e) {
-            e.preventDefault();
-            $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');                
-        });            
-    });
