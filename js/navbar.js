5aedac3e1f1abcc3bb93f7426484bf55a9b05298
ajadvien@ucalgary.ca <ajadvien@ucalgary.ca>
2016-06-24 14:34:25 -0600

    more tests

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-    var url = window.location;
-    $('.navbar .nav').find('.active').removeClass('active');
-    $('.navbar .nav li a').each(function () {
-        if (this.href == url) {
-			$(this).parent().addClass('active');
-		}
-    }); 
-});
+        $('ul.nav > li').click(function (e) {
+            e.preventDefault();
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');                
+        });            
+    });
